e5970ab9a128781316320267f09ffe5d7b396beb
wachira140 <wachiraw424@gmail>
2023-05-04 16:35:14 +0300

    fix bugs

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+    country CHAR(2) NOT NULL ENUM('US', 'CO', 'TN')
 );
--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,7 @@
 -- Create a trigger to decrease the quantity of an item
 -- after adding a new order
 DROP TRIGGER IF EXISTS decrease_quantity
-DELIMITER ##
+DELIMITER $$
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
@@ -9,5 +9,5 @@ BEGIN
     UPDATE items
         SET quantity = quantity - NEW.number
         WHERE name = NEW.item_name;
-END ##
+END $$
 DELIMITER ;
